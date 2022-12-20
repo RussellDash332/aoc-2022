2a348fe350e329fb66e95bbfff41102b1912324c
RussellDash332 <russellsaerang@gmail.com>
2022-12-20 12:54:38 +0700

    Tidy up Day 20

--- Day-20/Python/main.py
+++ Day-20/Python/main.py
@@ -4,9 +4,6 @@ arr = []
 for line in sys.stdin:
     arr.append((int(line), len(arr)))
     if arr[-1][0] == 0: ii = arr[-1][1]
-arr2 = list(map(lambda x: (811589153*x[0], x[1]), arr))
-order = arr.copy()
-order2 = arr2.copy()
 
 def mix(arr, order):
     n = len(arr) - 1
@@ -16,10 +13,12 @@ def mix(arr, order):
         arr.pop(t)
         arr.insert((t + i[0]) % n, i)
 
-def solve(arr, order, it):
+def solve(arr, mult, it):
+    arr = [(a*mult, b) for a,b in arr]
+    order = arr.copy()
     for _ in range(it): mix(arr, order)
     z = arr.index((0, ii))
     return sum(arr[(z + 1000*(i+1)) % len(arr)][0] for i in range(3))
 
-print('Part 1:', solve(arr, order, 1))
-print('Part 2:', solve(arr2, order2, 10))
+print('Part 1:', solve(arr, 1, 1))
+print('Part 2:', solve(arr, 811589153, 10))
